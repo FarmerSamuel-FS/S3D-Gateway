da3f8c7022308c14d05abc3da669d1c1a5851ca6
FarmerSamuel-FS <sfarmer1@student.fullsail.edu>
2024-06-25 14:25:33 -0400

    Basic file Structure fixed

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,25 @@
-import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import Header from "./components/common/Header";
-import Footer from "./components/common/Footer";
-import Navigation from "./components/common/Navigation";
-import Home from "./components/pages/Home";
-import PrintStatus from "./components/pages/PrintStatus";
-import About from "./components/pages/About";
-import Subscriptions from "./components/pages/Subscriptions";
-import UserSettings from "./components/pages/UserSettings";
+import logo from './logo.svg';
+import './App.css';
 
-const App = () => {
+function App() {
   return (
-    <Router>
-      <div className="App">
-        <Header />
-        <Navigation />
-        <main className="main-content">
-          <Router>
-            <Route exact path="/" component={Home} />
-            <Route path="/print-status" component={PrintStatus} />
-            <Route path="/about" component={About} />
-            <Route path="/subscriptions" component={Subscriptions} />
-            <Route path="/user-settings" component={UserSettings} />
-          </Router>
-        </main>
-        <Footer />
-      </div>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
-};
+}
 
 export default App;
